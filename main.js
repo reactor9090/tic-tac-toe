ad49cc4f77a7d475e53c669523c072a1c35a6c93
Max <monaco2093@gmail.com>
2025-04-11 14:54:22 +0300

    Made the board display information right. Made the 'playRound' function that plays the game and takes inputs from players

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
 
-
-
-const PlayingTable = {
-    gameboard: ["", "", "", "", "", "", "", "", ""]
+const GameBoard = (function () {
+    const board = ["", "", "", "", "", "", "", "", ""];
+ 
+    const getBoard = () => board;
+ 
+    return {getBoard};
+})();
+ 
+ 
+function Player (name, symbol) {
+    return {name, symbol}
 }
-
-
-function Player (player, symbol) {
-    return {player, symbol}
-}
-
-
+ 
 const GameController = (function () {
-       
-    const board = PlayingTable.gameboard;
-        
-        const getBoard = () => board;
+    let board = GameBoard.getBoard();
+
+    let player1 = Player('P1', 'X');
+    let player2 = Player('P2', 'O');
+
+    let activePlayer = player1;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === player1 ? player2 : player1;
+    }
+
+    const playRound = () => {
+        let promptResult = prompt(`${activePlayer.name} enter the cell: `);
+        promptResult = Number(promptResult)
+
+        if (promptResult === 1) {
+            board[0] = (activePlayer.symbol);
+            switchPlayerTurn();
+            console.log('done')
+            console.log(board);
+        } 
+    }
     
-        return {
-            consolelogBoard: () => {
-                console.log(getBoard());
-            }
-        }
-})
+    playRound();
+    playRound();
 
-GameController();
-
-(function DisplayBoard() {
-    GameController.consolelogBoard();
+    return {
+        consolelogBoard: () => {
+            console.log(board);
+        },    
+    }   
 }())
 
+// Now everything is good! (check out in console);
+// Now we need to add a "avaialableCells" function that will verify which cells are empty
+// and are okay to put a symbol in.
 
- 
+
+
+function displayGameController () {
+    GameController.consolelogBoard();
+}
+
